46d3d898ba0cf86d2f5c98e41a67691d9a9204b2
matakagi <matakagi@ncsu.edu>
2016-03-27 15:29:07 -0700

    Update anomaly.py
    Add some function outlines.

--- anomaly.py
+++ anomaly.py
@@ -11,6 +11,23 @@ def read_file(filename) :
     f = open(filename, "r")
     for line in f:
         yield line
+        
+def doc2L(doc):
+    """
+    Initially, a document d is transformed to a set of weighted features L = {(ti, wi)} where feature ti
+    is a token of d and wi is its frequency in d. Tokens are also obtained as in shingling and appear only 
+    once in set L. This weighted set can be viewed as a multidimensional vector. 
+    """
+    # FILL IN CODE HERE
+        
+def simhash(L1, L2):
+    """
+    simhash from equation (6): simhash(L1,L2) = 1 - hamming(h,h')/b
+    """
+    # FILL IN CODE HERE
+    
+    
+
 
 
 def main():
